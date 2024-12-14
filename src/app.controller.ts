92e710c65561a208e901d5ef4a6388e77f64a6e1
khaleda-02 <khaleda.02f@gmail.com>
2024-12-14 11:11:32 +0200

    test pushing on academex org

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,6 +14,7 @@ export class AppController {
   @Public()
   @Get('/.well-known/assetlinks.json')
   deepLink() {
+    
     return [
       {
         relation: ['delegate_permission/common.handle_all_urls'],
